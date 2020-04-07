c69d5f65358a76f94a0dadf2d06c67062004de71
Boris Legkun <dizamino@gmail.com>
2020-04-08 01:33:43 +0300

    auth server test fixed

--- src/main/kotlin/AuthServerApi.kt
+++ src/main/kotlin/AuthServerApi.kt
@@ -1,10 +1,11 @@
 import boris.project.utils.info.response.Info
 import feign.RequestLine
+import net.minidev.json.JSONObject
 
 interface AuthServerApi {
-  @RequestLine("GET /ping")
+  @RequestLine("GET /public/ping")
   fun ping(): Info
 
-  @RequestLine("GET /whoami")
-  fun whoami(): String
+  @RequestLine("GET /.well-known/jwks.json")
+  fun getJwkKeys(): JSONObject
 }
--- src/test/kotlin/boris/project/tests/auth/server/AuthServerTests.kt
+++ src/test/kotlin/boris/project/tests/auth/server/AuthServerTests.kt
@@ -11,51 +11,51 @@ import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.core.isSuccessful
 import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.fuel.jackson.responseObject
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.boot.SpringBootConfiguration
-import org.springframework.http.HttpStatus
 
 @SpringBootConfiguration
 class AuthServerTests {
 
-    //  private val authServerClient =
-    //      FeignFactory().create(AuthServerApi::class.java, "http://localhost:8081/sso-auth-server")
+  private val authServerClient =
+      FeignFactory().create(AuthServerApi::class.java, "http://localhost:8081/sso-auth-server")
 
-    @Test
-    fun `get token by client_credentials grant type`() {
-        val authServerClient =
-                FeignFactory().createWithClientCredentials(
-                        AuthServerApi::class.java,
-                        "http://localhost:8080/")
-        val res = authServerClient.whoami()
-        assertEquals("boris-client", res)
-    }
+  @Test
+  fun `jwks endpoint accessible`() {
+    val response = authServerClient.getJwkKeys()
+    assert(response.containsKey("keys"))
+    val keys = response["keys"] as ArrayList<*>
+    assertThat(keys.size > 0)
+  }
 
-    @Test
-    fun `auth server is alive - ping returns info that has status up`() {
-        val authServerClient =
-                FeignFactory().createWithClientCredentials(AuthServerApi::class.java, "http://localhost:8081/sso-auth-server/public")
-        val serverInfo = authServerClient.ping()
-        assertEquals(serverInfo.status, "up")
-    }
+  @Test
+  fun `auth server is alive - ping returns info that server has status up`() {
+    val serverInfo = authServerClient.ping()
+    assertEquals(serverInfo.status, "up")
+  }
 
-    @Test
-    fun `auth server returns token on succesfull authorize`() {
-        val mapper = ObjectMapper().registerKotlinModule()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+  @Test
+  fun `auth server returns token on succesfull authorize`() {
+    val mapper = ObjectMapper().registerKotlinModule()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
-        mapper.propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
-        val (request, response, result) = "http://localhost:8081/sso-auth-server/oauth/token?grant_type=client_credentials&scope=any"
-                .httpPost().authentication().basic("boris-client", "boris-secret").responseObject<TokenResponse>(mapper)
-        val (token, error) = result
-        assert(response.isSuccessful)
-        assert(token!!.accessToken.isNotEmpty())
-    }
+    mapper.propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+    val (request, response, result) = "http://localhost:8081/sso-auth-server/oauth/token?grant_type=client_credentials&scope=any"
+        .httpPost()
+        .authentication()
+        .basic("boris-client", "boris-secret")
+        .responseObject<TokenResponse>(mapper)
+    val (token, error) = result
+    assert(response.isSuccessful)
+    assert(token!!.accessToken.isNotEmpty())
+  }
 }
 
-class TokenResponse(@JsonProperty("access_token") val accessToken: String,
-                    @JsonProperty("token_type") val tokenType: String,
-                    @JsonProperty("expires_in") val expires: Int,
-                    val scope: String, val jti: String)
+class TokenResponse(
+  @JsonProperty("access_token") val accessToken: String,
+  @JsonProperty("token_type") val tokenType: String,
+  @JsonProperty("expires_in") val expires: Int,
+  val scope: String, val jti: String
+)
